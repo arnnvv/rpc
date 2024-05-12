2794d2a885e440a25c8556ed0975f4e1659c204f
arnnvv <arnav.900@protonmail.com>
2024-05-13 02:21:34 +0530

    types

--- grpc.ts
+++ grpc.ts
@@ -1,42 +1,56 @@
 import path from "path";
 import * as grpc from "@grpc/grpc-js";
-import type { ServiceClientConstructor } from "@grpc/grpc-js";
 import * as protoLoader from "@grpc/proto-loader";
+import { ProtoGrpcType } from "./generated/a";
+import { AddressBookServiceHandlers } from "./generated/AddressBookService";
+import { Status } from "@grpc/grpc-js/build/src/constants";
 
 const packageDefinition = protoLoader.loadSync(
   path.join(__dirname, "./a.proto"),
 );
 
-const personProto = grpc.loadPackageDefinition(packageDefinition);
+const personProto = grpc.loadPackageDefinition(
+  packageDefinition,
+) as unknown as ProtoGrpcType;
 
-const PERSONS = [
+const PEOPLE = [
   {
-    name: "harkirat",
-    age: 45,
+    name: "Arnav",
+    age: 20,
   },
   {
-    name: "raman",
-    age: 45,
+    name: "Haana",
+    age: 20,
   },
 ];
 
-//@ts-ignore
-function addPerson(call, callback) {
-  console.log(call);
-  let person = {
-    name: call.request.name,
-    age: call.request.age,
-  };
-  PERSONS.push(person);
-  callback(null, person);
-}
-
 const server = new grpc.Server();
 
-server.addService(
-  (personProto.AddressBookService as ServiceClientConstructor).service,
-  { addPerson: addPerson },
-);
+const handlers: AddressBookServiceHandlers = {
+  AddPerson: (call, callback) => {
+    console.log(call);
+    let person = {
+      name: call.request.name,
+      age: call.request.age,
+    };
+    PEOPLE.push(person);
+    callback(null, person);
+  },
+  GetPersonByName: (call, callback) => {
+    let person = PEOPLE.find((p) => p.name === call.request.name);
+    if (person) callback(null, person);
+    else
+      callback(
+        {
+          code: Status.NOT_FOUND,
+          details: "Person not found",
+        },
+        null,
+      );
+  },
+};
+
+server.addService(personProto.AddressBookService.service, handlers);
 server.bindAsync(
   "0.0.0.0:50051",
   grpc.ServerCredentials.createInsecure(),
